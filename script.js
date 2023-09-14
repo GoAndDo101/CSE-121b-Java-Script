01bc9d0bf24ab5aa1e9f3f9c368fbefcd5cf21ba
GoAndDo101 <144616455+GoAndDo101@users.noreply.github.com>
2023-09-14 15:48:30 -0600

    reset some stuff

--- script.js
+++ script.js
@@ -10,6 +10,3 @@ function createParagraph() {
     button.addEventListener("click", createParagraph);
   }
 
-document.querySelector("html").addEventListener("click", function () {
-    alert("Ouch! Stop poking me!");
-});
